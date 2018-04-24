190fb72cb2b201292629dc4e03d3a4e6532069e4
Marco Nicola <marconicola@disroot.org>
2018-04-24 23:34:09 +0200

    Add BaseSentenceElement class

---
+++ colonel/base_sentence_element.py
@@ -0,0 +1,45 @@
+
+"""Module providing the :class:`.BaseSentenceElement` class."""
+
+from typing import Optional
+
+__all__ = ['BaseSentenceElement']
+
+
+class BaseSentenceElement:
+    """Abstract class containing the minimum information in common with
+    all more specific elements being part of a sentence.
+
+    In the context of this library, it is expected that each item of a sentence
+    is an instance of a :class:`.BaseSentenceElement` subclass (or, in rare
+    cases, at least an instance of :class:`.BaseSentenceElement` itself).
+
+    The generic term *element* is used in order to prevent confusion, while
+    each specialized element (i.e. a subclass of :class:`.BaseSentenceElement`)
+    will adopt a more appropriate nomenclature, so that, for example, a
+    sentence will be formed by *words*, a *tokens* or *nodes*.
+    """
+
+    __slots__ = ('form', 'misc')
+
+    def __init__(self, form: Optional[str] = None, misc: Optional[str] = None):
+        #: Form of the word.
+        #:
+        #: It is compatible with *CoNLL-U* ``FORM`` field.
+        self.form: Optional[str] = form
+
+        #: Miscellaneous word's annotations.
+        #:
+        #: It is compatible with *CoNLL-U* ``MISC`` field.
+        self.misc: Optional[str] = misc
+
+    @staticmethod
+    def is_valid() -> bool:
+        """Returns whether or not the object can be considered valid,
+        without considering the context of the sentence in which the word
+        itself is possibly inserted.
+
+        An instance of type :class:`.BaseWord` is **always** considered valid,
+        independently from the value of :attr:`.form` and :attr:`.misc`.
+        """
+        return True
