b6796e747b22d26db424142987641ba5ac8e6119
Marco Nicola <marconicola@disroot.org>
2018-04-28 17:28:10 +0200

    Fix too many newlines in conllu.lexer module

--- colonel/conllu/lexer.py
+++ colonel/conllu/lexer.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 """Module providing the :class:`.ConlluLexerBuilder` class and related
 exception classes.
 """
